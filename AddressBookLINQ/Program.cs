512e713c6d8d5ee84bb2ad62a0db2cf9bdc9d8ec
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-08 20:10:08 +0530

    [sahithi] creating a data table

---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AddressBook Service Using LINQ");
+        }
+    }
+}
